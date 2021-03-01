e5610530ee7df4f726d681ed54364d7f8162d5dd
Anastasiya <anabasanets@gmail.com>
2021-03-01 17:24:28 -0500

    add brute force algo

--- Divide_And_Conquer.cpp
+++ Divide_And_Conquer.cpp
@@ -3,170 +3,256 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <algorithm>
 
-using namespace std; 
-  
+using namespace std;
+
 // A structure for building 
-struct Building { 
+struct Building {
     // x coordinate of left side 
-    int left; 
-  
+    int left;
+
     // x coordinate of right side 
     int right;
 
     // height 
-    int height; 
-}; 
-  
+    int height;
+};
+
 // A strip in skyline 
-class Strip { 
+class Strip {
     // x coordinate of left side 
-    int left; 
-  
+    int left;
+
     // height 
-    int height; 
-  
-public: 
-    Strip(int l = 0, int h = 0) 
-    { 
-        left = l; 
-        height = h; 
-    } 
-    friend class SkyLine; 
-}; 
-  
+    int height;
+
+public:
+    Strip(int l = 0, int h = 0)
+    {
+        left = l;
+        height = h;
+    }
+    friend class SkyLine;
+};
+
+struct Point {
+    int x, y;
+    Point(int paramx, int paramy) : x(paramx), y(paramy) {}
+};
+
+
+
 // Skyline: To represent Output(An array of strips) 
-class SkyLine { 
+class SkyLine {
     // Array of strips 
-    Strip* arr; 
-  
+    Strip* arr;
+
     // Capacity of strip array 
-    int capacity; 
-  
+    int capacity;
+
     // Actual number of strips in array 
-    int n; 
-  
-public: 
-    ~SkyLine() { delete[] arr; } 
-    int count() { return n; } 
-  
+    int n;
+
+public:
+    ~SkyLine() { delete[] arr; }
+    int count() { return n; }
+
     // A function to merge another skyline to this skyline 
-    SkyLine* Merge(SkyLine* other); 
-  
+    SkyLine* Merge(SkyLine* other);
+
     // Constructor 
-    SkyLine(int cap) 
-    { 
-        capacity = cap; 
-        arr = new Strip[cap]; 
-        n = 0; 
-    } 
-  
+    SkyLine(int cap)
+    {
+        capacity = cap;
+        arr = new Strip[cap];
+        n = 0;
+    }
+
     // Function to add a strip 'st' to array 
-    void append(Strip* st) 
-    { 
+    void append(Strip* st)
+    {
         // Check for redundant strip, a strip is 
         // redundant if it has same height or left as previous 
-        if (n > 0 && arr[n - 1].height == st->height) 
-            return; 
-        if (n > 0 && arr[n - 1].left == st->left) { 
-            arr[n - 1].height = max(arr[n - 1].height, st->height); 
-            return; 
-        } 
-  
-        arr[n] = *st; 
-        n++; 
-    } 
-  
+        if (n > 0 && arr[n - 1].height == st->height)
+            return;
+        if (n > 0 && arr[n - 1].left == st->left) {
+            arr[n - 1].height = max(arr[n - 1].height, st->height);
+            return;
+        }
+
+        arr[n] = *st;
+        n++;
+    }
+
     // A utility function to print all strips of skyline 
-    void print() 
-    { 
-        for (int i = 0; i < n; i++) { 
+    void print()
+    {
+        for (int i = 0; i < n; i++) {
             cout << " (" << arr[i].left << ", "
-                 << arr[i].height << "), "; 
-        } 
-    } 
-}; 
-  
+                << arr[i].height << "), ";
+        }
+    }
+};
+
 // This function returns skyline for a given array of buildings arr[l..h]. 
 // This function is similar to mergeSort(). 
-SkyLine* divideAndConquer(Building arr[], int l, int h) 
-{ 
-    if (l == h) { 
-        SkyLine* res = new SkyLine(2); 
-        res->append(new Strip(arr[l].left, arr[l].height)); 
-        res->append(new Strip(arr[l].right, 0)); 
-        return res; 
-    } 
-  
-    int mid = (l + h) / 2; 
-  
+SkyLine* divideAndConquer(Building arr[], int l, int h)
+{
+    if (l == h) {
+        SkyLine* res = new SkyLine(2);
+        res->append(new Strip(arr[l].left, arr[l].height));
+        res->append(new Strip(arr[l].right, 0));
+        return res;
+    }
+
+    int mid = (l + h) / 2;
+
     // Recur for left and right halves and merge the two results 
-    SkyLine* sl = divideAndConquer(arr, l, mid); 
-    SkyLine* sr = divideAndConquer(arr, mid + 1, h); 
-    SkyLine* res = sl->Merge(sr); 
-  
+    SkyLine* sl = divideAndConquer(arr, l, mid);
+    SkyLine* sr = divideAndConquer(arr, mid + 1, h);
+    SkyLine* res = sl->Merge(sr);
+
     // To avoid memory leak 
-    delete sl; 
-    delete sr; 
-  
+    delete sl;
+    delete sr;
+
     // Return merged skyline 
-    return res; 
-} 
-  
+    return res;
+}
+
 // Similar to merge() in MergeSort 
 // This function merges another skyline 'other' to the skyline for which it is called. 
 // The function returns pointer to the resultant skyline 
-SkyLine* SkyLine::Merge(SkyLine* other) 
-{ 
+SkyLine* SkyLine::Merge(SkyLine* other)
+{
     // Create a resultant skyline with capacity as sum of two skylines 
-    SkyLine* res = new SkyLine( 
-        this->n + other->n); 
-  
+    SkyLine* res = new SkyLine(
+        this->n + other->n);
+
     // To store current heigheights of two skylines 
-    int h1 = 0, h2 = 0; 
-  
+    int h1 = 0, h2 = 0;
+
     // Indexes of strips in two skylines 
-    int i = 0, j = 0; 
-    while (i < this->n && j < other->n) { 
+    int i = 0, j = 0;
+    while (i < this->n && j < other->n) {
         // Compare x coordinates of left sides of two skylines and put the smaller one in result 
-        if (this->arr[i].left < other->arr[j].left) { 
-            int x1 = this->arr[i].left; 
-            h1 = this->arr[i].height; 
-  
+        if (this->arr[i].left < other->arr[j].left) {
+            int x1 = this->arr[i].left;
+            h1 = this->arr[i].height;
+
             // Choose height as max of two heigheights 
-            int maxh = max(h1, h2); 
-  
-            res->append(new Strip(x1, maxh)); 
-            i++; 
-        } 
-        else { 
-            int x2 = other->arr[j].left; 
-            h2 = other->arr[j].height; 
-            int maxh = max(h1, h2); 
-            res->append(new Strip(x2, maxh)); 
-            j++; 
-        } 
-    } 
-  
+            int maxh = max(h1, h2);
+
+            res->append(new Strip(x1, maxh));
+            i++;
+        }
+        else {
+            int x2 = other->arr[j].left;
+            h2 = other->arr[j].height;
+            int maxh = max(h1, h2);
+            res->append(new Strip(x2, maxh));
+            j++;
+        }
+    }
+
     // If there are strips left in this skyline or other skyline 
-    while (i < this->n) { 
-        res->append(&arr[i]); 
-        i++; 
-    } 
-    while (j < other->n) { 
-        res->append(&other->arr[j]); 
-        j++; 
-    } 
-    return res; 
-} 
+    while (i < this->n) {
+        res->append(&arr[i]);
+        i++;
+    }
+    while (j < other->n) {
+        res->append(&other->arr[j]);
+        j++;
+    }
+    return res;
+}
+
+bool compare(Point i1, Point i2)
+{
+    return (i1.x < i2.x);
+}
+
+int substract(int x, int y) {
+    int result = x - y;
+    return result;
+    
+}
+int add(int x, int y) {
+    int result = x + y;
+    return result;
+}
+
+vector<Point> checkLinearity(vector<Point> points) {
+    for (unsigned int i = 0; i < points.size() - 1; i++) {
+        if (substract(points[i + 1].x, points[i].x) > 1) {
+            points.insert(points.begin() + i + 1, Point(add(points[i].x, 1), 0));
+        }
+    }
+    return points;
+}
+
+
+
+vector<Point> bruteForce(Building arr[], int n) {
+    std::vector<Point> points;
+    for (int i= 0; i < n; i++) {
+        int first_x = arr[i].left;
+        int last_x = arr[i].right;
+        for (int j = first_x; j < last_x+1; j++) {
+            points.push_back(Point(j, arr[i].height));
+            
+        }
+    }
+    std::sort(points.begin(), points.end(),compare);
+    std::vector<Point> currentPoint = {Point (0,0),Point(1,0) };
+    std::vector<Point> sortedPoints;
+    //find for same x max y value
+    for (unsigned int i = 0; i < points.size();i++) {
+        if (points[i].x > currentPoint[0].x) {
+            sortedPoints.push_back(Point(currentPoint[0].x, currentPoint[0].y));
+            currentPoint[0].x = points[i].x;
+            currentPoint[0].y = points[i].y;
+        }
+        if (points[i].x == currentPoint[0].x) {
+            if (points[i].y > currentPoint[0].y) {
+                currentPoint[0].y = points[i].y;
+            }
+        }
+        
+    }
+    sortedPoints.push_back(Point(points[points.size()-1].x, 0));
+
+    //check linearity to have a (x,y) for all range of x
+    sortedPoints = checkLinearity(sortedPoints);
+    
+    //find skyline
+    std::vector<Point> finalPoints;
+    for (unsigned int i = 0; i < sortedPoints.size()-1; i++) {
+        if (currentPoint[1].y != sortedPoints[i].y) {
+            if (currentPoint[1].y > sortedPoints[i].y) {
+                    finalPoints.push_back(Point(sortedPoints[i - 1].x, sortedPoints[i].y));
+                    currentPoint[1].y = sortedPoints[i].y;
+            }
+            else {
+                finalPoints.push_back(Point(sortedPoints[i].x, sortedPoints[i].y));
+                currentPoint[1].y = sortedPoints[i].y;
+            }
+        }
+       
+    }
+    finalPoints.push_back(Point(sortedPoints[sortedPoints.size() - 1].x, sortedPoints[sortedPoints.size() - 1].y));
+    return finalPoints;
+    
+}
 
 static void showUsage(std::string name)
 {
     std::cerr << "Usage: " << name << " -a {brute, recursif, seuil} -e CHEMIN_EXEMPLAIRE <option(s)> SOURCES"
-              << "Options:\n"
-              << "\t[-p] affiche, sur chaque ligne, les couples définissant la silhouette de bâtiments, triés selon l’abscisse et sans texte superflu (les deux valeurs d’un couple sont séparées d’un espace)\n"
-              << "\t[-t] affiche le temps d’exécution en millisecondes, sans unité ni texte superflu\n"
-              << std::endl;
+        << "Options:\n"
+        << "\t[-p] affiche, sur chaque ligne, les couples définissant la silhouette de bâtiments, triés selon l’abscisse et sans texte superflu (les deux valeurs d’un couple sont séparées d’un espace)\n"
+        << "\t[-t] affiche le temps d’exécution en millisecondes, sans unité ni texte superflu\n"
+        << std::endl;
 }
 
 std::pair<int, std::vector<Building>> readExempFile(std::string filePath)
@@ -176,34 +262,40 @@ std::pair<int, std::vector<Building>> readExempFile(std::string filePath)
     std::string line;
     bool isFirstLine = true;
     int nbrBuildings = 0;
-    if(inputFileStream){
+    if (inputFileStream) {
         while (std::getline(inputFileStream, line)) {
-            if(isFirstLine) {
+            if (isFirstLine) {
                 nbrBuildings = std::stoi(line);
                 cout << " n is :" << nbrBuildings << endl;
                 isFirstLine = false;
-            } else {
+                int right, left, height; // on doit le faire une fois pour prendre la premiere coordonnée
+                inputFileStream >> right >> left >> height;
+                Building building = { right, left, height };
+                buildingArray.push_back(building);
+            }
+            else {
                 int right, left, height;
                 inputFileStream >> right >> left >> height;
-                Building building = {right, left, height};
+                Building building = { right, left, height };
                 buildingArray.push_back(building);
             }
         }
         inputFileStream.close();
-    } else{
+    }
+    else {
         std::cerr << "Couldn't open " << filePath << " for reading\n";
     }
     return std::make_pair(nbrBuildings, buildingArray);
 }
 
 // Driver Function 
-int main(int argc, char* argv[]) 
-{ 
+int main(int argc, char* argv[])
+{
     if (argc < 5) {
         showUsage(argv[0]);
         return 1;
     }
-    
+
     std::string method;
     std::string filePath;
 
@@ -211,29 +303,40 @@ int main(int argc, char* argv[])
         std::string arg = argv[i];
         if (arg == "-a") {
             method = argv[++i];
-        } else if (arg == "-e") {
+        }
+        else if (arg == "-e") {
             filePath = argv[++i];
-        } else {
+        }
+        else {
             //implement -p and -t options if we have time
         }
     }
 
     std::pair<int, std::vector<Building>> fileData = readExempFile(filePath);
     int n = fileData.first;
-    Building* arr = new Building [fileData.second.size()];
+    Building* arr = new Building[fileData.second.size()];
     std::copy(fileData.second.begin(), fileData.second.end(), arr);
 
-    if(method == "recursif") {
+    if (method == "recursif") {
         // Find skyline for given buildings and print the skyline 
-        SkyLine* ptr = divideAndConquer(arr, 0, n - 1); 
-        cout << " Skyline for given buildings is \n"; 
+        SkyLine* ptr = divideAndConquer(arr, 0, n - 1);
+        cout << " Skyline for given buildings is \n";
         ptr->print();
-    } else if (method == "brute"){
+    }
+    else if (method == "brute") {
+        std::vector<Point> skyline = bruteForce(arr, n);
+        cout << " Skyline for given buildings is" << endl;
+        for (unsigned int i = 0; i < skyline.size(); i++) {
+            cout << '('<< skyline[i].x <<","<<skyline[i].y<<')'<< endl;
+        }
+    
         //call brute force algo 
-    } else if (method == "seuil"){
+    }
+    else if (method == "seuil") {
         //call seuil algo
-    } else {
+    }
+    else {
         std::cerr << "Invalid algorithm type \n";
     }
-    return 0; 
-} 
+    return 0;
+}
