ee54c949dae18750284723364af66062a7d44d46
MariamSarwat <37277152+MariamSarwat@users.noreply.github.com>
2021-03-03 22:51:01 -0500

    Add comments, Fix print, add print and time parameters

--- Divide_And_Conquer.cpp
+++ Divide_And_Conquer.cpp
@@ -1,15 +1,14 @@
-// A divide and conquer based C++ program to find skyline of given buildings 
 #include <iostream> 
 #include <fstream>
 #include <vector>
 #include <string>
 #include <algorithm>
-#include <ctime>
 #include <chrono>
 
 using namespace std;
-int init_time;
-int final_time;
+
+// A divide and conquer based C++ program to find skyline of given buildings 
+// https://www.geeksforgeeks.org/the-skyline-problem-using-divide-and-conquer-algorithm/
 
 // A structure for building 
 struct Building {
@@ -39,13 +38,6 @@ public:
     }
     friend class SkyLine;
 };
-
-struct Point {
-    int x, y;
-    Point(int paramx, int paramy) : x(paramx), y(paramy) {}
-};
-
-
 
 // Skyline: To represent Output(An array of strips) 
 class SkyLine {
@@ -93,8 +85,7 @@ public:
     void print()
     {
         for (int i = 0; i < n; i++) {
-            cout << " (" << arr[i].left << ", "
-                << arr[i].height << "), ";
+            cout << arr[i].left << " " << arr[i].height << endl;
         }
     }
 };
@@ -131,8 +122,7 @@ SkyLine* divideAndConquer(Building arr[], int l, int h)
 SkyLine* SkyLine::Merge(SkyLine* other)
 {
     // Create a resultant skyline with capacity as sum of two skylines 
-    SkyLine* res = new SkyLine(
-        this->n + other->n);
+    SkyLine* res = new SkyLine(this->n + other->n);
 
     // To store current heigheights of two skylines 
     int h1 = 0, h2 = 0;
@@ -150,8 +140,7 @@ SkyLine* SkyLine::Merge(SkyLine* other)
 
             res->append(new Strip(x1, maxh));
             i++;
-        }
-        else {
+        } else {
             int x2 = other->arr[j].left;
             h2 = other->arr[j].height;
             int maxh = max(h1, h2);
@@ -171,25 +160,35 @@ SkyLine* SkyLine::Merge(SkyLine* other)
     }
     return res;
 }
+// End of divide and Conquer code.
 
 
+// Brute force program to find skyline of given buildings 
+// Inspired from : http://sandrasi-sw.blogspot.com/2012/12/the-skyline-problem.html
+struct Point 
+{
+    int x, y;
+    Point(int paramx, int paramy) : x(paramx), y(paramy) {}
+};
 
 bool compare(Point i1, Point i2)
 {
     return (i1.x < i2.x);
 }
 
-int substract(int x, int y) {
+int substract(int x, int y) 
+{
     int result = x - y;
     return result;
-
 }
+
 int add(int x, int y) {
     int result = x + y;
     return result;
 }
 
-vector<Point> checkLinearity(vector<Point> points) {
+vector<Point> checkLinearity(vector<Point> points) 
+{
     for (unsigned int i = 0; i < points.size() - 1; i++) {
         if (substract(points[i + 1].x, points[i].x) > 1) {
             points.insert(points.begin() + i + 1, Point(add(points[i].x, 1), 0));
@@ -198,21 +197,21 @@ vector<Point> checkLinearity(vector<Point> points) {
     return points;
 }
 
-
-
-vector<Point> bruteForce(Building arr[], int n) {
+vector<Point> bruteForce(Building arr[], int n) 
+{
     std::vector<Point> points;
     for (int i = 0; i < n; i++) {
         int first_x = arr[i].left;
         int last_x = arr[i].right;
         for (int j = first_x; j < last_x + 1; j++) {
             points.push_back(Point(j, arr[i].height));
-
         }
     }
+
     std::sort(points.begin(), points.end(), compare);
     std::vector<Point> currentPoint = { Point(0,0),Point(1,0) };
     std::vector<Point> sortedPoints;
+    
     //find for same x max y value
     for (unsigned int i = 0; i < points.size(); i++) {
         if (points[i].x > currentPoint[0].x) {
@@ -225,7 +224,6 @@ vector<Point> bruteForce(Building arr[], int n) {
                 currentPoint[0].y = points[i].y;
             }
         }
-
     }
     sortedPoints.push_back(Point(points[points.size() - 1].x, 0));
 
@@ -245,57 +243,61 @@ vector<Point> bruteForce(Building arr[], int n) {
                 currentPoint[1].y = sortedPoints[i].y;
             }
         }
-
     }
     finalPoints.push_back(Point(sortedPoints[sortedPoints.size() - 1].x, sortedPoints[sortedPoints.size() - 1].y));
     return finalPoints;
-
 }
 
-
-
-SkyLine* convertStripToSkyLine(vector<Point> points) {
+SkyLine* convertStripToSkyLine(vector<Point> points) 
+{
     SkyLine* skyline = new SkyLine(points.size());
     for (unsigned i = 0; i < points.size(); i++) {
         skyline->append(new Strip(points[i].x, points[i].y));
     }
     return skyline;
 }
+// End of brute force code.
 
-SkyLine* divideAndConquerSeuil(Building arr[], int l, int h)
+
+// Divide and Conquer with resursion threshold.
+SkyLine* divideAndConquerSeuil(Building arr[], int l, int h) 
 {
-    int n = sizeof(arr);
+    int n = h + 1;
     if (n <= 20) {
         vector<Point> result = bruteForce(arr, h);
         SkyLine* skyline = convertStripToSkyLine(result);
         return skyline;
     }
 
-    SkyLine* skyline = divideAndConquer(arr, 0, n - 1);
+    SkyLine* skyline = divideAndConquer(arr, 0, h);
     return skyline;
 }
+// End Divide and Conquer with resursion threshold code.
 
-static void showUsage(std::string name)
+
+// Print help menu
+static void showUsage(std::string name) 
 {
     std::cerr << "Usage: " << name << " -a {brute, recursif, seuil} -e CHEMIN_EXEMPLAIRE <option(s)> SOURCES"
-        << "Options:\n"
+        << "Parametre optionel pa:\n"
         << "\t[-p] affiche, sur chaque ligne, les couples définissant la silhouette de bâtiments, triés selon l’abscisse et sans texte superflu (les deux valeurs d’un couple sont séparées d’un espace)\n"
         << "\t[-t] affiche le temps d’exécution en millisecondes, sans unité ni texte superflu\n"
         << std::endl;
 }
 
-std::pair<int, std::vector<Building>> readExempFile(std::string filePath)
+// Read File with building coordinates
+std::pair<int, std::vector<Building>> readExempFile(std::string filePath) 
 {
     std::ifstream inputFileStream(filePath);
     std::vector<Building> buildingArray;
     std::string line;
     bool isFirstLine = true;
     int nbrBuildings = 0;
+
     if (inputFileStream) {
         while (std::getline(inputFileStream, line)) {
             if (isFirstLine) {
                 nbrBuildings = std::stoi(line);
-                cout << " n is :" << nbrBuildings << endl;
                 isFirstLine = false;
             }
 
@@ -305,15 +307,14 @@ std::pair<int, std::vector<Building>> readExempFile(std::string filePath)
             buildingArray.push_back(building);
         }
         inputFileStream.close();
-    }
-    else {
-        std::cerr << "Couldn't open " << filePath << " for reading\n";
+    } else {
+        std::cerr << "Ne peux pas ouvrir le fichier specifie " << filePath << "\n";
     }
     return std::make_pair(nbrBuildings, buildingArray);
 }
 
-// Driver Function 
-int main(int argc, char* argv[])
+// Main function
+int main(int argc, char* argv[]) 
 {
     if (argc < 5) {
         showUsage(argv[0]);
@@ -322,17 +323,18 @@ int main(int argc, char* argv[])
 
     std::string method;
     std::string filePath;
+    bool printResult = false, printTime = false;
 
     for (int i = 1; i < argc; ++i) {
         std::string arg = argv[i];
         if (arg == "-a") {
             method = argv[++i];
-        }
-        else if (arg == "-e") {
+        } else if (arg == "-e") {
             filePath = argv[++i];
-        }
-        else {
-            //implement -p and -t options if we have time
+        } else if (arg == "-p") {
+            printResult = true;
+        } else if (arg == "-t") {
+            printTime = true;
         }
     }
 
@@ -341,35 +343,38 @@ int main(int argc, char* argv[])
     Building* arr = new Building[fileData.second.size()];
     std::copy(fileData.second.begin(), fileData.second.end(), arr);
 
-    if (method == "recursif") {
-        // Find skyline for given buildings and print the skyline
-        auto start = std::chrono::high_resolution_clock::now();
-        SkyLine* ptr = divideAndConquer(arr, 0, n - 1);
-        ptr->print();
-        auto finish = std::chrono::high_resolution_clock::now();
-        std::cout << std::chrono::duration_cast<std::chrono::nanoseconds>(finish - start).count() << "ns\n";
-    }
-    else if (method == "brute") {
-        //call brute force algo 
-        
+    SkyLine* ptr;
+    double executionTime;
 
+    if (method == "recursif") {
+        auto start = std::chrono::high_resolution_clock::now();
+        ptr = divideAndConquer(arr, 0, n - 1);
+        auto finish = std::chrono::high_resolution_clock::now();
+        executionTime = std::chrono::duration<double>(finish - start).count();  
+    } 
+    else if (method == "brute") {
         auto start = std::chrono::high_resolution_clock::now();
         vector<Point> skyline = (bruteForce(arr, n - 1));
-        SkyLine* ptr = convertStripToSkyLine(skyline);
-        ptr->print();
+        ptr = convertStripToSkyLine(skyline);
         auto finish = std::chrono::high_resolution_clock::now();
-        std::cout << std::chrono::duration_cast<std::chrono::nanoseconds>(finish - start).count() << "ns\n";
-    }
+        executionTime = std::chrono::duration<double>(finish - start).count();    
+    } 
     else if (method == "seuil") {
-        //call seuil algo
         auto start = std::chrono::high_resolution_clock::now();
-        SkyLine* ptr = divideAndConquerSeuil(arr, 0, n - 1);
-        ptr->print();
+        ptr = divideAndConquerSeuil(arr, 0, n - 1);
         auto finish = std::chrono::high_resolution_clock::now();
-        std::cout << std::chrono::duration_cast<std::chrono::nanoseconds>(finish - start).count() << "ns\n";
-    }
+        executionTime = std::chrono::duration<double>(finish - start).count();
+    } 
     else {
-        std::cerr << "Invalid algorithm type \n";
+        std::cerr << "Algorithme de type invalide \n";
+        return 1;
+    }
+
+    if (printResult) {
+        ptr->print();
+    }
+    if (printTime) {
+        std::cout << executionTime << " ms\n";
     }
     return 0;
 }
