c2c52304b1f8d656a398a33fd482f19c4e4b6b7c
MariamSarwat <37277152+MariamSarwat@users.noreply.github.com>
2021-03-03 23:06:00 -0500

    Add p and t arguments in tp.sh file

--- tp.sh
+++ tp.sh
@@ -3,13 +3,15 @@
 # https://www.baeldung.com/linux/use-command-line-arguments-in-bash-script
 # https://archive.is/TRzn4
 
-while getopts a:e: flag
+while getopts a:e:pt flag
 do
     case "${flag}" in
         a) algoType=${OPTARG};;
         e) filePath=${OPTARG};;
+        p) print="-p";;
+        t) printTime="-t";;
     esac
 done
 
 g++ Divide_And_Conquer.cpp -o main 
-./main -a "$algoType" -e "$filePath"
+./main -a "$algoType" -e "$filePath" $print $printTime
