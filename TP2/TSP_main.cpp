12649e5d110e58aee90870bad8d12bf232d105d1
MariamSarwat <37277152+MariamSarwat@users.noreply.github.com>
2021-03-23 22:45:10 -0400

    Add modified read file method and add greedy algo (Needs to be modified)

--- TP2/TSP_main.cpp
+++ TP2/TSP_main.cpp
@@ -7,6 +7,74 @@
 
 using namespace std;
 
+// A structure for x and y coordinates 
+struct Coord {
+    int x;
+    int y;
+};
+
+#include <bits/stdc++.h>
+// Function to find the minimum cost path for all the paths
+void findMinRoute(vector<vector<int> > tsp)
+{
+	int sum = 0;
+	int counter = 0;
+	int j = 0, i = 0;
+	int min = INT_MAX;
+	map<int, int> visitedRouteList;
+
+	// Starting from the 0th indexed city i.e., the first city
+	visitedRouteList[0] = 1;
+	int route[tsp.size()];
+
+	// Traverse the adjacency matrix tsp[][]
+	while (i < tsp.size() && j < tsp[i].size())
+	{
+		// Corner of the Matrix
+		if (counter >= tsp[i].size() - 1) break;
+
+		// If this path is unvisited then and if the cost is less then update the cost
+		if (j != i && (visitedRouteList[j] == 0))
+		{
+			if (tsp[i][j] < min)
+			{
+				min = tsp[i][j];
+				route[counter] = j + 1;
+			}
+		}
+		j++;
+
+		// Check all paths from the ith indexed city
+		if (j == tsp[i].size())
+		{
+			sum += min;
+			min = INT_MAX;
+			visitedRouteList[route[counter] - 1] = 1;
+			j = 0;
+			i = route[counter] - 1;
+			counter++;
+		}
+	}
+
+	// Update the ending city in array from city which was last visited
+	i = route[counter - 1] - 1;
+
+	for (j = 0; j < tsp.size(); j++)
+	{
+		if ((i != j) && tsp[i][j] < min)
+		{
+			min = tsp[i][j];
+			route[counter] = j + 1;
+		}
+	}
+	sum += min;
+
+	// Started from the node where we finished as well.
+	std::cout << ("Minimum Cost is : ");
+	std::cout << (sum) << std::endl;
+}
+// This code is contributed by grand_master.
+
 // Print help menu
 static void showUsage(std::string name) 
 {
@@ -16,33 +84,35 @@ static void showUsage(std::string name)
         << "\t[-t] affiche le temps d’exécution en millisecondes, sans unité ni texte superflu\n"
         << std::endl;
 }
-/*
-// Read File with building coordinates
-std::pair<int, std::vector<Building>> readExempFile(std::string filePath) 
+
+// Read File with City coordinates
+std::pair<int, std::vector<Coord>> readExempFile(std::string filePath) 
 {
     std::ifstream inputFileStream(filePath);
-    std::vector<Building> buildingArray;
+    std::vector<Coord> cityArr;
     std::string line;
+
     bool isFirstLine = true;
-    int nbrBuildings = 0;
+    int nbrCities = 0;
+
     if (inputFileStream) {
         while (std::getline(inputFileStream, line)) {
             if (isFirstLine) {
-                nbrBuildings = std::stoi(line);
+                nbrCities = std::stoi(line);
                 isFirstLine = false;
             }
 
-            int right, left, height;
-            inputFileStream >> right >> left >> height;
-            Building building = { right, left, height };
-            buildingArray.push_back(building);
+            int x, y;
+            inputFileStream >> x >> y;
+            Coord coordinates = { x, y };
+            cityArr.push_back(coordinates);
         }
         inputFileStream.close();
     } else {
         std::cerr << "Ne peux pas ouvrir le fichier specifie " << filePath << "\n";
     }
-    return std::make_pair(nbrBuildings, buildingArray);
-}*/
+    return std::make_pair(nbrCities, cityArr);
+}
 
 // Main function
 int main(int argc, char* argv[]) 
@@ -68,18 +138,24 @@ int main(int argc, char* argv[])
             printTime = true;
         }
     }
-/*
-    std::pair<int, std::vector<Building>> fileData = readExempFile(filePath);
-    int n = fileData.first;
-    Building* arr = new Building[fileData.second.size()];
-    std::copy(fileData.second.begin(), fileData.second.end(), arr);
 
-    SkyLine* ptr;
-*/
+    std::pair<int, std::vector<Coord>> fileData = readExempFile(filePath);
+    int nbrCities = fileData.first;
+    std::vector<Coord> cityArr = fileData.second;
+
     double executionTime;
+
     if (method == "glouton") {
         auto start = std::chrono::high_resolution_clock::now();
         //call algo
+        // Input Matrix
+        vector<vector<int> > tsp = { { -1, 10, 15, 20 },
+                                    { 10, -1, 35, 25 },
+                                    { 15, 35, -1, 30 },
+                                    { 20, 25, 30, -1 } };
+
+        // Function Call
+	    findMinRoute(tsp);
         auto finish = std::chrono::high_resolution_clock::now();
         executionTime = std::chrono::duration<double, std::milli>(finish - start).count();  
     } 
