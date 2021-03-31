03608b4529e615b3fd1a616be3cbe83e8d2dd919
Anastasiya <anabasanets@gmail.com>
2021-03-31 17:09:28 -0400

    AB: Modify approx algo with primMST

--- TP2/TSP_main.cpp
+++ TP2/TSP_main.cpp
@@ -23,6 +23,8 @@ bool operator == (Coord point1, Coord point2) {
     return (point1.x == point2.x) && (point1.y == point2.y);
 }
 
+
+
 uint64_t findEuclDist(Coord initCoord, Coord finalCoord) {
     uint64_t x = pow(finalCoord.x - initCoord.x, 2);
     uint64_t y = pow(finalCoord.y - initCoord.y, 2);
@@ -149,7 +151,7 @@ void getDPPath(int pos, int mask, const vector<vector<int>>& citiesMatrix, const
 
 //https://www.geeksforgeeks.org/travelling-salesman-problem-set-2-approximate-using-mst/?fbclid=IwAR1mcT7xs8ARV-fgmI7uAbIRTN1buN6i2ANK2HKPoEn3f6zvjG9Tkc-otgA
 //http://www.mathcs.emory.edu/~cheung/Courses/171/Syllabus/11-Graph/prim2.html?fbclid=IwAR03NsIwqkDZUYQam7pWxX7dccTySCZobjWRxNshCH94tQrR2RmRXqRM5vs
-void approx(std::vector<Coord> cityArr, vector<int>& shortestPath) {
+void approx_old(std::vector<Coord> cityArr, vector<int>& shortestPath) {
     uint64_t totMinDist = 0;
     int counter = 0;
 
@@ -157,12 +159,12 @@ void approx(std::vector<Coord> cityArr, vector<int>& shortestPath) {
     Coord actualPoint = {};
     Coord chosenPoint = {};
     Coord firstPoint = {};
-    vector<Coord> cityArrRef=cityArr;
+    vector<Coord> cityArrRef = cityArr;
     std::vector<Coord> visitedRouteList;
 
     actualPoint.x = cityArr[0].x;
     actualPoint.y = cityArr[0].y;
-    firstPoint.x= cityArr[0].x;
+    firstPoint.x = cityArr[0].x;
     firstPoint.y = cityArr[0].y;
     shortestPath.push_back(0);
 
@@ -196,6 +198,290 @@ void approx(std::vector<Coord> cityArr, vector<int>& shortestPath) {
     totMinDist += distEucl;
     visitedRouteList.push_back(firstPoint);
     std::cout << "Minimum Cost is : " << (totMinDist) << std::endl;
+
+}
+
+struct AdjListNode {
+    int dest;
+    int weight;
+    struct AdjListNode* next;
+};
+
+// A structure to represent an adjacency list
+struct AdjList {
+    struct AdjListNode* head; // pointer to head node of list
+};
+
+// A structure to represent a graph. A graph is an array of adjacency lists.
+// Size of array will be V (number of vertices in graph)
+struct Graph {
+    int V;
+    struct AdjList* array;
+};
+
+// A utility function to create a new adjacency list node
+struct AdjListNode* newAdjListNode(int dest, int weight)
+{
+    struct AdjListNode* newNode = (struct AdjListNode*)malloc(sizeof(struct AdjListNode));
+    newNode->dest = dest;
+    newNode->weight = weight;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// A utility function that creates a graph of V vertices
+struct Graph* createGraph(int V)
+{
+    struct Graph* graph = (struct Graph*)malloc(sizeof(struct Graph));
+    graph->V = V;
+
+    // Create an array of adjacency lists.  Size of array will be V
+    graph->array = (struct AdjList*)malloc(V * sizeof(struct AdjList));
+
+    // Initialize each adjacency list as empty by making head as NULL
+    for (int i = 0; i < V; ++i)
+        graph->array[i].head = NULL;
+
+    return graph;
+}
+
+// Adds an edge to an undirected graph
+void addEdge(struct Graph* graph, int src, int dest, int weight)
+{
+    // Add an edge from src to dest.  A new node is added to the adjacency
+    // list of src.  The node is added at the beginning
+    struct AdjListNode* newNode = newAdjListNode(dest, weight);
+    newNode->next = graph->array[src].head;
+    graph->array[src].head = newNode;
+
+    // Since graph is undirected, add an edge from dest to src also
+    newNode = newAdjListNode(src, weight);
+    newNode->next = graph->array[dest].head;
+    graph->array[dest].head = newNode;
+}
+
+// Structure to represent a min heap node
+struct MinHeapNode {
+    int v;
+    int key;
+};
+
+// Structure to represent a min heap
+struct MinHeap {
+    int size; // Number of heap nodes present currently
+    int capacity; // Capacity of min heap
+    int* pos; // This is needed for decreaseKey()
+    struct MinHeapNode** array;
+};
+
+// A utility function to create a new Min Heap Node
+struct MinHeapNode* newMinHeapNode(int v, int key)
+{
+    struct MinHeapNode* minHeapNode = (struct MinHeapNode*)malloc(sizeof(struct MinHeapNode));
+    minHeapNode->v = v;
+    minHeapNode->key = key;
+    return minHeapNode;
+}
+
+// A utilit function to create a Min Heap
+struct MinHeap* createMinHeap(int capacity)
+{
+    struct MinHeap* minHeap = (struct MinHeap*)malloc(sizeof(struct MinHeap));
+    minHeap->pos = (int*)malloc(capacity * sizeof(int));
+    minHeap->size = 0;
+    minHeap->capacity = capacity;
+    minHeap->array = (struct MinHeapNode**)malloc(capacity * sizeof(struct MinHeapNode*));
+    return minHeap;
+}
+
+// A utility function to swap two nodes of min heap. Needed for min heapify
+void swapMinHeapNode(struct MinHeapNode** a, struct MinHeapNode** b)
+{
+    struct MinHeapNode* t = *a;
+    *a = *b;
+    *b = t;
+}
+
+// A standard function to heapify at given idx
+// This function also updates position of nodes when they are swapped.
+// Position is needed for decreaseKey()
+void minHeapify(struct MinHeap* minHeap, int idx)
+{
+    int smallest, left, right;
+    smallest = idx;
+    left = 2 * idx + 1;
+    right = 2 * idx + 2;
+
+    if (left < minHeap->size && minHeap->array[left]->key < minHeap->array[smallest]->key)
+        smallest = left;
+
+    if (right < minHeap->size && minHeap->array[right]->key < minHeap->array[smallest]->key)
+        smallest = right;
+
+    if (smallest != idx) {
+        // The nodes to be swapped in min heap
+        MinHeapNode* smallestNode = minHeap->array[smallest];
+        MinHeapNode* idxNode = minHeap->array[idx];
+
+        // Swap positions
+        minHeap->pos[smallestNode->v] = idx;
+        minHeap->pos[idxNode->v] = smallest;
+
+        // Swap nodes
+        swapMinHeapNode(&minHeap->array[smallest], &minHeap->array[idx]);
+
+        minHeapify(minHeap, smallest);
+    }
+}
+
+// A utility function to check if the given minHeap is ampty or not
+int isEmpty(struct MinHeap* minHeap)
+{
+    return minHeap->size == 0;
+}
+
+// Standard function to extract minimum node from heap
+struct MinHeapNode* extractMin(struct MinHeap* minHeap)
+{
+    if (isEmpty(minHeap))
+        return NULL;
+
+    // Store the root node
+    struct MinHeapNode* root = minHeap->array[0];
+
+    // Replace root node with last node
+    struct MinHeapNode* lastNode = minHeap->array[minHeap->size - 1];
+    minHeap->array[0] = lastNode;
+
+    // Update position of last node
+    minHeap->pos[root->v] = minHeap->size - 1;
+    minHeap->pos[lastNode->v] = 0;
+
+    // Reduce heap size and heapify root
+    --minHeap->size;
+    minHeapify(minHeap, 0);
+
+    return root;
+}
+
+// Function to decrease key value of a given vertex v. This function
+// uses pos[] of min heap to get the current index of node in min heap
+void decreaseKey(struct MinHeap* minHeap, int v, int key)
+{
+    // Get the index of v in  heap array
+    int i = minHeap->pos[v];
+
+    // Get the node and update its key value
+    minHeap->array[i]->key = key;
+
+    // Travel up while the complete tree is not hepified.
+    // This is a O(Logn) loop
+    while (i && minHeap->array[i]->key < minHeap->array[(i - 1) / 2]->key) {
+        // Swap this node with its parent
+        minHeap->pos[minHeap->array[i]->v] = (i - 1) / 2;
+        minHeap->pos[minHeap->array[(i - 1) / 2]->v] = i;
+        swapMinHeapNode(&minHeap->array[i], &minHeap->array[(i - 1) / 2]);
+
+        // move to parent index
+        i = (i - 1) / 2;
+    }
+}
+
+// A utility function to check if a given vertex
+// 'v' is in min heap or not
+bool isInMinHeap(struct MinHeap* minHeap, int v)
+{
+    if (minHeap->pos[v] < minHeap->size)
+        return true;
+    return false;
+}
+
+// A utility function used to print the constructed MST
+void printArr(int arr[], int n)
+{
+    for (int i = 1; i < n; ++i)
+        printf("%d - %d\n", arr[i], i);
+}
+
+// The main function that constructs Minimum Spanning Tree (MST)
+// using Prim's algorithm
+void PrimMST(struct Graph* graph)
+{
+    uint64_t totMinDist = 0;
+    const int V = graph->V; // Get the number of vertices in graph
+    int* parent = new int[V];
+    for (int i = 0; i < V; i++)
+        parent[i] = i;
+    // Array to store constructed MST
+   //int key[V]; 
+    int* key = new int[V];
+    for (int i = 0; i < V; i++)
+        key[i] = i;// Key values used to pick minimum weight edge in cut
+
+    // minHeap represents set E
+    struct MinHeap* minHeap = createMinHeap(V);
+
+    // Initialize min heap with all vertices. Key value of
+    // all vertices (except 0th vertex) is initially infinite
+    for (int v = 1; v < V; ++v) {
+        parent[v] = -1;
+        key[v] = INT_MAX;
+        minHeap->array[v] = newMinHeapNode(v, key[v]);
+        minHeap->pos[v] = v;
+    }
+
+    // Make key value of 0th vertex as 0 so that it
+    // is extracted first
+    key[0] = 0;
+    minHeap->array[0] = newMinHeapNode(0, key[0]);
+    minHeap->pos[0] = 0;
+
+    // Initially size of min heap is equal to V
+    minHeap->size = V;
+
+    // In the following loop, min heap contains all nodes
+    // not yet added to MST.
+    while (!isEmpty(minHeap)) {
+        // Extract the vertex with minimum key value
+        struct MinHeapNode* minHeapNode = extractMin(minHeap);
+        int u = minHeapNode->v; // Store the extracted vertex number
+
+        // Traverse through all adjacent vertices of u (the extracted
+        // vertex) and update their key values
+        struct AdjListNode* pCrawl = graph->array[u].head;
+        while (pCrawl != NULL) {
+            int v = pCrawl->dest;
+
+            // If v is not yet included in MST and weight of u-v is
+            // less than key value of v, then update key value and
+            // parent of v
+            if (isInMinHeap(minHeap, v) && pCrawl->weight < key[v]) {
+                key[v] = pCrawl->weight;
+                
+
+                parent[v] = u;
+                decreaseKey(minHeap, v, key[v]);
+            }
+            pCrawl = pCrawl->next;
+        }
+        totMinDist += key[u];
+    }
+
+    // print edges of MST
+    printArr(parent, V);
+    std::cout << totMinDist << endl;
+} 
+
+Graph* createGraph(std::vector<Coord> cityArr) {
+    int V = cityArr.size();
+    struct Graph* graph = createGraph(V);
+    for (int i = 0; i < cityArr.size(); i++) {
+        for (int j = i + 1; j < cityArr.size(); j++) {
+            addEdge(graph, i, j, findEuclDist(cityArr[i], cityArr[j]));
+        }
+    }
+
+    return graph;
     
 }
 
@@ -240,17 +526,18 @@ std::pair<int, std::vector<Coord>> readExempFile(std::string filePath)
     return std::make_pair(nbrCities, cityArr);
 }
 
-void printShortestPath(vector<int> shortestPath){
+void printShortestPath(vector<int> shortestPath) {
     shortestPath.push_back(0);
-    if(shortestPath[1] > shortestPath[shortestPath.size() - 2]){
+    if (shortestPath[1] > shortestPath[shortestPath.size() - 2]) {
         for (int k = shortestPath.size() - 1; k >= 0; k--) {
             std::cout << shortestPath[k] << endl;
         }
-    } else {
+    }
+    else {
         for (int k = 0; k < shortestPath.size(); k++) {
             std::cout << shortestPath[k] << endl;
         }
-    } 
+    }
 }
 
 // Main function
@@ -295,8 +582,9 @@ int main(int argc, char* argv[])
         getDPPath(0, 1, citiesMatrix, state, shortestPath);
     }
     else if (method == "approx" || method == APPROX_CODE) {
+        Graph* graph = createGraph(cityArr);
         start = std::chrono::high_resolution_clock::now();
-        approx(cityArr, shortestPath);
+        PrimMST(graph);
         finish = std::chrono::high_resolution_clock::now();
     }
 
